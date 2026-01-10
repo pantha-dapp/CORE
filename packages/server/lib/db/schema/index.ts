6b28e7f9242994cac6dc08839c020949bb7953cb
Spandan Barve <spandan567@gmail.com>
2026-01-10 15:47:32 +0530

    schema index exports

---
+++ packages/server/lib/db/schema/index.ts
@@ -0,0 +1,21 @@
+import * as course from "./course";
+
+// Combine all schema parts
+const schema = {
+	...course,
+};
+
+export default schema;
+
+// type DBSchema = typeof schema;
+// export type DB = {
+//   [K in keyof DBSchema as K extends `${infer Base}s` // Tables typically end with 's'
+//     ? Base // Standard table name (convert 'users' to 'user', etc.)
+//     : K extends UtilityFunctions // Exclude utility functions
+//     ? never
+//     : K]: K extends keyof DBSchema
+//     ? DBSchema[K] extends { $inferSelect: any }
+//       ? DBSchema[K]["$inferSelect"]
+//       : never
+//     : never;
+// };
