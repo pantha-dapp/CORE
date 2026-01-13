bef5f68a55f067ff4431144802138e8f575c87df
Spandan Barve <spandan567@gmail.com>
2026-01-13 18:08:17 +0530

    feat: initialize server with Hono framework and CORS support

---
+++ packages/server/index.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { apiRouter } from "./api/routes/router";
+import env from "./env";
+
+//@ts-expect-error
+BigInt.prototype.toJSON = function () {
+	return this.toString();
+};
+
+export const app = new Hono()
+	.use(logger())
+	.use(
+		cors({
+			origin: [env.FRONTEND_URL],
+			allowHeaders: ["Content-Type", "Authorization"],
+			allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+			credentials: true,
+		}),
+	)
+	.route("/api", apiRouter);
+
+export default {
+	port: Bun.env.PORT ? parseInt(Bun.env.PORT, 10) : 30013,
+	fetch: app.fetch,
+};
