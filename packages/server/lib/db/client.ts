5071f40f8eb4175dec0abfa3e1dbedbda6d82161
Spandan Barve <spandan567@gmail.com>
2026-01-14 19:58:47 +0530

    feat: switch database client to SQLite using bun and drizzle ORM

--- packages/server/lib/db/client.ts
+++ packages/server/lib/db/client.ts
@@ -1,10 +1,16 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { SQL } from "bun"
+import { Database } from "bun:sqlite"
+import { drizzle } from "drizzle-orm/bun-sql";
+import * as sqliteVec from "sqlite-vec";
 import env from "../../env";
 import schema from "./schema";
 
-const dbClient = drizzle(env.PG_URI.replace(":dbname", env.DB_NAME), {
-	schema,
-	casing: "snake_case",
+const client = new SQL(env.SQLITE_FILE_PATH);
+const db = new Database(env.SQLITE_FILE_PATH);
+
+const dbClient = drizzle(client, {
+    schema,
+    casing: "snake_case",
 });
 
 export default dbClient;
