a4e2100b56f373874fc7b096aa05bc2f2facedb9
Spandan Barve <spandan567@gmail.com>
2026-01-15 12:39:26 +0530

    add mising env var for sqlite

--- packages/server/env.ts
+++ packages/server/env.ts
@@ -1,10 +1,9 @@
 const envKeys = [
-	"PG_URI",
-	"DB_NAME",
-	"FRONTEND_URL",
-	"GROQ_API_KEY",
-	"TG_ANALYTICS_BOT_TOKEN",
-	"TG_ANALYTICS_BOT_GROUP_ID",
+    "FRONTEND_URL",
+    "GROQ_API_KEY",
+    "SQLITE_FILE_PATH",
+    "TG_ANALYTICS_BOT_TOKEN",
+    "TG_ANALYTICS_BOT_GROUP_ID",
 ] as const;
 
 type ENV = Record<(typeof envKeys)[number], string>;
@@ -12,16 +11,16 @@ type ENV = Record<(typeof envKeys)[number], string>;
 let env: ENV = {} as ENV;
 
 export function ensureEnv() {
-	for (const key of envKeys) {
-		if (!Bun.env[key]) {
-			throw new Error(`Environment variable ${key} is not set`);
-		}
-	}
+    for (const key of envKeys) {
+        if (!Bun.env[key]) {
+            throw new Error(`Environment variable ${key} is not set`);
+        }
+    }
 
-	env = Object.fromEntries(envKeys.map((key) => [key, Bun.env[key]])) as ENV;
+    env = Object.fromEntries(envKeys.map((key) => [key, Bun.env[key]])) as ENV;
 }
 const isProd =
-	process.env.NODE_ENV === "production" || process.env.NODE_ENV === "prod";
+    process.env.NODE_ENV === "production" || process.env.NODE_ENV === "prod";
 if (!isProd) ensureEnv();
 
 export default env;
