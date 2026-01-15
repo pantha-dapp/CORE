5a942ff7aa85ac3cfb8c6590135b4df770b2be33
Spandan Barve <spandan567@gmail.com>
2026-01-15 17:10:00 +0530

    fix: reorder database client initialization for proper SQLite loading

--- packages/server/lib/db/client.ts
+++ packages/server/lib/db/client.ts
@@ -5,8 +5,11 @@ import * as sqliteVec from "sqlite-vec";
 import env from "../../env";
 import schema from "./schema";
 
-const client = new SQL(env.SQLITE_FILE_PATH);
 const db = new Database(env.SQLITE_FILE_PATH);
+sqliteVec.load(db)
+db.close();
+
+const client = new SQL(env.SQLITE_FILE_PATH);
 
 const dbClient = drizzle(client, {
     schema,
