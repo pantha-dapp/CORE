8705c1ebaa8e8b204e5cc15d0d985984d616f29e
Spandan Barve <spandan567@gmail.com>
2026-01-16 23:26:15 +0530

    feat: refactor database client initialization for improved compatibility

--- packages/server/lib/db/client.ts
+++ packages/server/lib/db/client.ts
@@ -1,17 +1,14 @@
 import { Database } from "bun:sqlite";
-import { SQL } from "bun";
-import { drizzle } from "drizzle-orm/bun-sql";
+import { drizzle } from "drizzle-orm/bun-sqlite";
 import * as sqliteVec from "sqlite-vec";
 import env from "../../env";
 import schema from "./schema";
 
 const db = new Database(env.SQLITE_FILE_PATH);
 sqliteVec.load(db);
-db.close();
 
-const client = new SQL(env.SQLITE_FILE_PATH);
-
-const dbClient = drizzle(client, {
+const dbClient = drizzle({
+	client: db,
 	schema,
 	casing: "snake_case",
 });
