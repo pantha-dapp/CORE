1c9d4e4b792ad1432dc85565f5c551c08666d855
Spandan Barve <spandan567@gmail.com>
2026-01-16 23:26:48 +0530

    refactor: improve code formatting and consistency in respond utility functions

--- packages/server/lib/utils/respond.ts
+++ packages/server/lib/utils/respond.ts
@@ -2,49 +2,49 @@ import { jsonStringify } from "@pantha/shared";
 import type { Context } from "hono";
 import type { ResponseHeader } from "hono/utils/headers";
 import type {
-    ClientErrorStatusCode,
-    ServerErrorStatusCode,
+	ClientErrorStatusCode,
+	ServerErrorStatusCode,
 } from "hono/utils/http-status";
 import type { BaseMime } from "hono/utils/mime";
 import type { JSONObject } from "hono/utils/types";
 
 export const respond = {
-    ok: <
-        C extends Context,
-        T extends JSONObject | Record<string, unknown>,
-        U extends 200 | 201 | 202 | 303,
-    >(
-        ctx: C,
-        data: T,
-        message: string,
-        status: U,
-        headers?: HeaderRecord,
-    ) => {
-        ctx.status(status);
-        for (const [name, value] of Object.entries(headers || {})) {
-            ctx.header(name, value);
-        }
-        return ctx.json({ success: true, data, message }); //status,headers);
-    },
+	ok: <
+		C extends Context,
+		T extends JSONObject | Record<string, unknown>,
+		U extends 200 | 201 | 202 | 303,
+	>(
+		ctx: C,
+		data: T,
+		message: string,
+		status: U,
+		headers?: HeaderRecord,
+	) => {
+		ctx.status(status);
+		for (const [name, value] of Object.entries(headers || {})) {
+			ctx.header(name, value);
+		}
+		return ctx.json({ success: true, data, message }); //status,headers);
+	},
 
-    err: <
-        C extends Context,
-        U extends ClientErrorStatusCode | ServerErrorStatusCode,
-    >(
-        ctx: C,
-        message: string,
-        status: U,
-        headers?: HeaderRecord,
-    ) => {
-        ctx.status(status);
-        for (const [name, value] of Object.entries(headers || {})) {
-            ctx.header(name, value);
-        }
-        return ctx.json({ success: false, error: jsonStringify(message) }); //, status, headers);
-    },
+	err: <
+		C extends Context,
+		U extends ClientErrorStatusCode | ServerErrorStatusCode,
+	>(
+		ctx: C,
+		message: string,
+		status: U,
+		headers?: HeaderRecord,
+	) => {
+		ctx.status(status);
+		for (const [name, value] of Object.entries(headers || {})) {
+			ctx.header(name, value);
+		}
+		return ctx.json({ success: false, error: jsonStringify(message) }); //, status, headers);
+	},
 };
 
 type HeaderRecord =
-    | Record<"Content-Type", BaseMime>
-    | Record<ResponseHeader, string>
-    | Record<string, string>;
+	| Record<"Content-Type", BaseMime>
+	| Record<ResponseHeader, string>
+	| Record<string, string>;
