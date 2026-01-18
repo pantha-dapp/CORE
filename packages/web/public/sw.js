7417b2c1decfc2d1fece4ad4967fc226091d9799
Spandan Barve <spandan567@gmail.com>
2026-01-18 16:43:02 +0530

    Add HTML entry point and public assets for web package

---
+++ packages/web/public/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'pantha-v1';
+const urlsToCache = [
+  '/',
+  '/src/main.tsx',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter(cacheName => cacheName !== CACHE_NAME).map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
