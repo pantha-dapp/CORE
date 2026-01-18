27fae2d92c9c360a777bac4ac8731e309716bef9
Spandan Barve <spandan567@gmail.com>
2026-01-19 04:09:01 +0530

    Add course routes to main API router

--- packages/server/api/routes/router.ts
+++ packages/server/api/routes/router.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
+import course from "./course";
 
-export const apiRouter = new Hono().get("/runtime", (ctx) => {
-	const runtime = {
-		uptime: process.uptime(),
-		// serverAddressSynapse: config.serverAddressSynapse,
-		// chain: config.runtimeChain,
-	};
-	return ctx.json(runtime);
-});
-// .route("/tx", tx);
+export const apiRouter = new Hono()
+	.get("/runtime", (ctx) => {
+		const runtime = {
+			uptime: process.uptime(),
+			// serverAddressSynapse: config.serverAddressSynapse,
+			// chain: config.runtimeChain,
+		};
+		return ctx.json(runtime);
+	})
+	.route("/course", course);
