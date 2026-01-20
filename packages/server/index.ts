d906b3fccf02125823cc81f8bfd496412e2fd3f2
Spandan Barve <spandan567@gmail.com>
2026-01-20 15:18:44 +0530

    Update server index.ts with new configuration

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,28 +1,14 @@
 import "dotenv/config";
-import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { logger } from "hono/logger";
-import { apiRouter } from "./api/routes/router";
-import env from "./env";
+import env, { ensureEnv } from "./env";
+
+ensureEnv();
+
+import { app } from "./server";
 
 //@ts-expect-error
 BigInt.prototype.toJSON = function () {
 	return this.toString();
 };
-
-export const app = new Hono()
-	.use(logger())
-	.use(
-		cors({
-			origin: [env.FRONTEND_URL],
-			allowHeaders: ["Content-Type", "Authorization"],
-			allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-			credentials: true,
-		}),
-	)
-	.route("/api", apiRouter);
-
-export type ApiType = typeof app;
 
 export default {
 	port: env.PORT ? parseInt(env.PORT, 10) : 31001,
