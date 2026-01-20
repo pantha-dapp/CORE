3b952253cacf4526cf0894504b7fbb3a0b6b202c
Spandan Barve <spandan567@gmail.com>
2026-01-20 15:18:24 +0530

    Update server constants file

--- packages/server/constants.ts
+++ packages/server/constants.ts
@@ -1,19 +1,15 @@
-import { keccak256 } from "viem";
+import { HOUR, MB } from "@pantha/shared/constants";
+import { hexToBytes, keccak256 } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import env from "./env";
-
-export const MINUTE = 60 * 1000;
-export const HOUR = 60 * MINUTE;
-export const DAY = 24 * HOUR;
-
-export const KB = 1024;
-export const MB = 1024 * KB;
 
 export const MAX_FILE_SIZE = 30 * MB;
 
 export const JWTalgorithm = "HS512";
 export const JWTexpiration = (3 * HOUR) / 1000; // 3 hours
-const JWTPrivateKey = keccak256(Uint8Array.from(env.EVM_PRIVATE_KEY_SYNAPSE));
+const JWTPrivateKey = keccak256(
+	hexToBytes(env.EVM_PRIVATE_KEY_SYNAPSE as `0x${string}`),
+);
 const JWTPublicKey = privateKeyToAccount(JWTPrivateKey).publicKey;
 export const JWTKeypair = { private: JWTPrivateKey, public: JWTPublicKey };
 
