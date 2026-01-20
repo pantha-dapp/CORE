3cff95b87cb290837548ef50610cb185a73dffe0
Spandan Barve <spandan567@gmail.com>
2026-01-20 15:18:17 +0530

    Update shared constants with time constants

--- packages/lib/shared/constants.ts
+++ packages/lib/shared/constants.ts
@@ -3,3 +3,6 @@ export const MINUTE = 60 * SECOND;
 export const HOUR = 60 * MINUTE;
 export const DAY = 24 * HOUR;
 export const WEEK = 7 * DAY;
+
+export const KB = 1024;
+export const MB = 1024 * KB;
