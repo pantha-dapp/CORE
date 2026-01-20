d77ee60740c8fa21b94dfb7ed50336d9b83ced1f
Spandan Barve <spandan567@gmail.com>
2026-01-20 15:51:47 +0530

    Add vite config for react-sdk package

---
+++ packages/lib/react-sdk/vite.config.ts
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	plugins: [react()],
+	build: {
+		lib: {
+			entry: "index.ts",
+			formats: ["es"],
+			fileName: "index",
+		},
+		rollupOptions: {
+			external: ["react", "react-dom"],
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+				},
+			},
+		},
+	},
+});
