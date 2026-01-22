261ee487343942a539e446edd6f05614d1ddb808
Spandan Barve <spandan567@gmail.com>
2026-01-22 22:31:30 +0530

    useenrollforcourse hook

---
+++ packages/lib/react-sdk/src/hooks/course/useEnrollForCourse.ts
@@ -0,0 +1,31 @@
+import { useMutation } from "@tanstack/react-query";
+import { usePanthaContext } from "../../context/PanthaProvider";
+
+export default function () {
+	const { wallet, api } = usePanthaContext();
+
+	return useMutation({
+		mutationFn: async (args: { courseId: string }) => {
+			const { courseId } = args;
+
+			if (!wallet) {
+				throw new Error("not connected");
+			}
+
+			const enrollResponseRaw = await api.rpc.course.enroll.$post({
+				json: {
+					courseId,
+				},
+			});
+			const enrollResponse = await enrollResponseRaw.json();
+
+			if (!enrollResponse.success) {
+				throw new Error("Failed to perform action", {
+					cause: enrollResponse.error,
+				});
+			}
+
+			return enrollResponse.data;
+		},
+	});
+}
