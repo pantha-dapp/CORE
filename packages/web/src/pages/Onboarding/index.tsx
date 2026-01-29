bf1637d2af60fc7cd78a97d2bb7d31792016b074
Abhishek Chaudhary <abhishek97208@gmail.com>
2026-01-30 01:56:45 +0530

    Refactor Onboarding component to improve layout and enhance job state handling

--- packages/web/src/pages/Onboarding/index.tsx
+++ packages/web/src/pages/Onboarding/index.tsx
@@ -4,18 +4,67 @@ import {
 	useCourseGenerationSession,
 } from "@pantha/react/hooks";
 
-export function Onboarding() {
+export default function Onboarding() {
 	const session = useCourseGenerationSession();
-	const action = useCourseGenerationAction();
+	const { data: jobState, mutate: action } = useCourseGenerationAction();
 	const majorCategories = useCourseGenerationMajorCategories();
 
 	return (
 		<div>
 			<textarea
 				rows={20}
-				className="resize-none text-sm"
+				className="resize-none text-sm p-5 w-full"
 				defaultValue={JSON.stringify(session.data, null, 2)}
 			/>
+			<textarea
+				rows={5}
+				className="resize-none text-sm p-5 w-full"
+				defaultValue={JSON.stringify({ jobState }, null, 2)}
+			/>
+
+			{session.data &&
+				(jobState?.state === "pending" ? (
+					"Loading wait"
+				) : (
+					<div className="flex flex-col gap-y-3">
+						{session.data.session.state === "major_category_choice" &&
+							majorCategories.data?.categories.map((cat, idx) => (
+								<button
+									key={cat}
+									type="button"
+									onClick={() =>
+										action({
+											action: {
+												type: "major_category_choice",
+												category: idx,
+											},
+										})
+									}
+								>
+									{cat}
+								</button>
+							))}
+
+						{session.data.session.state === "learning_intent_freetext" && (
+							<div className="flex flex-col gap-y-3">
+								<p>intent </p>
+								<button
+									type="button"
+									onClick={() =>
+										action({
+											action: {
+												type: "learning_intent_freetext",
+												intent: "I want to learn about this just for fun!",
+											},
+										})
+									}
+								>
+									submit a cool intent
+								</button>
+							</div>
+						)}
+					</div>
+				))}
 		</div>
 	);
 }
