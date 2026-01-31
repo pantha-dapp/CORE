5d56196a131f5b81d5fcdba2913180b3f5b83280
Abhishek Chaudhary <abhishek97208@gmail.com>
2026-01-31 22:52:16 +0530

    Update RPC utilities

--- packages/lib/react-sdk/src/utils/rpc.ts
+++ packages/lib/react-sdk/src/utils/rpc.ts
@@ -50,6 +50,10 @@ export default class ApiClient {
 		return this._client.api;
 	}
 
+	get ws() {
+		return this._client.ws;
+	}
+
 	setJwt(authToken: string | null) {
 		this._authHeader = { Authorization: `Bearer ${authToken}` };
 		this._client = this.createClient();
