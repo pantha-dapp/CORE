a38cc13c7b5c24fe518568711752d988f78d602d
Spandan Barve <spandan567@gmail.com>
2026-02-02 03:04:59 +0530

    Add useCourseChaptersByCourseId hook

---
+++ packages/lib/react-sdk/src/hooks/course/useCourseChaptersByCourseId.ts
@@ -0,0 +1,29 @@
+import { DAY } from "@pantha/shared/constants";
+import { useQuery } from "@tanstack/react-query";
+import { usePanthaContext } from "../../context/PanthaProvider";
+
+export function useCourseChaptersByCourseId(args: { courseId: string }) {
+	const { wallet, api } = usePanthaContext();
+	const { courseId } = args;
+
+	return useQuery({
+		queryKey: ["courseChaptersByCourseId", courseId],
+		queryFn: async () => {
+			if (!wallet) {
+				throw new Error("not connected");
+			}
+
+			const chaptersResponseRaw = await api.rpc.course[":id"].chapters.$get({
+				param: { id: courseId },
+			});
+			const chaptersResponse = await chaptersResponseRaw.json();
+
+			if (!chaptersResponse.success) {
+				throw new Error("Failed to retrieve chapters");
+			}
+
+			return chaptersResponse.data;
+		},
+		staleTime: 7 * DAY,
+	});
+}
