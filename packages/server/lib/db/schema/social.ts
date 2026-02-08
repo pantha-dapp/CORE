d543b8e81377a3a71602c1433117b3154518637a
Spandan Barve <spandan567@gmail.com>
2026-02-09 01:57:55 +0530

    Add social schema

---
+++ packages/server/lib/db/schema/social.ts
@@ -0,0 +1,9 @@
+import * as t from "drizzle-orm/sqlite-core";
+import { tEvmAddress, timestamps } from "../helpers.base";
+
+export const followings = t.sqliteTable("followings", {
+	follower: tEvmAddress().notNull(),
+	following: tEvmAddress().notNull(),
+
+	...timestamps,
+});
