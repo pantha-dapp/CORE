4c5892fc25cdcb15a9c7d34492bb161f4bdee222
Spandan Barve <spandan567@gmail.com>
2026-02-10 05:04:55 +0530

    Update server router

--- packages/server/api/routes/router.ts
+++ packages/server/api/routes/router.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
+import env from "../../env";
+import { attachDb } from "../middleware/attachDb";
 import auth from "./auth";
 import course from "./course";
 import jobs from "./jobs";
 
 export const apiRouter = new Hono()
+	.use(attachDb(env.SQLITE_FILE_PATH))
 	.get("/runtime", (ctx) => {
 		const runtime = {
 			uptime: process.uptime(),
