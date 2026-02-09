17486cf606ebbb93693c86e37722bf6715935c20
Spandan Barve <spandan567@gmail.com>
2026-02-10 05:05:12 +0530

    Update AI cache

--- packages/server/lib/ai/cache.ts
+++ packages/server/lib/ai/cache.ts
@@ -1,10 +1,6 @@
 import { jsonParse, jsonStringify } from "@pantha/shared";
 import { MINUTE } from "@pantha/shared/constants";
-import { count, eq, sql } from "drizzle-orm";
-import db from "../db";
-import { createVectorDbClient } from "../db/vec/client";
-
-const vectorDb = createVectorDbClient("llm-resp-vec-cache");
+import { createVectorDb, type VectorDbClient } from "../db/vec/client";
 
 const CACHE_EVICTION_TRIGGER_THRESHOLD = 500;
 const CACHE_ENTRIES_TO_EVICT = 50;
@@ -14,100 +10,93 @@ const cacheMeta = {
 	lastEvictionAt: 0,
 };
 
-async function cacheEviction() {
-	if (!cacheMeta.count || cacheMeta.count <= 0) {
-		const [currentCount] = await db
-			.select({ count: count() })
-			.from(db.schema.vectorCache);
-		if (!currentCount) {
-			cacheMeta.count = 0;
-			return;
-		}
-		cacheMeta.count = currentCount.count;
-	}
+export function createAiCache(vectorDbClient: VectorDbClient) {
+	const vectorDb = createVectorDb(vectorDbClient, "llm-resp-vec-cache");
 
-	if (cacheMeta.count >= CACHE_EVICTION_TRIGGER_THRESHOLD) {
-		if (Date.now() - cacheMeta.lastEvictionAt > 5 * MINUTE) {
-			await db.transaction(async (tx) => {
-				const entriesToEvict = await tx
-					.select()
-					.from(db.schema.vectorCache)
-					.orderBy(
-						// depeding on policy choose target column for ordering
-						CACHE_EVICTION_POLICY === "lfu"
-							? db.schema.vectorCache.hits
-							: db.schema.vectorCache.lastHitAt,
-					)
-					.limit(CACHE_ENTRIES_TO_EVICT);
-
-				for (const entry of entriesToEvict) {
-					cacheMeta.count -= 1;
-					await tx
-						.delete(db.schema.vectorCache)
-						.where(eq(db.schema.vectorCache.id, entry.id));
-					vectorDb.deleteEntry(entry.id);
-				}
-			});
-		}
-	}
-}
-
-export async function getCachedResponse<T>(
-	inputEmbedding: number[],
-	outputSchema: { safeParse: (data: unknown) => { success: boolean } },
-): Promise<T | null> {
-	const [cachedEntry] = await vectorDb.querySimilar(inputEmbedding, 1);
-	console.log(cachedEntry);
-	if (cachedEntry && cachedEntry.score > 0.998877) {
-		const [cachedResponse] = await db
-			.select()
-			.from(db.schema.vectorCache)
-			.where(eq(db.schema.vectorCache.id, cachedEntry.id));
-
-		if (cachedResponse) {
-			const content = cachedResponse.content;
-			const { success } = outputSchema.safeParse(jsonParse(content));
-
-			if (success) {
-				// register the cache hit but do not need to await it
-				db.update(db.schema.vectorCache)
-					.set({
-						lastHitAt: Date.now(),
-						hits: sql`${db.schema.vectorCache.hits} + 1`,
-					})
-					.where(eq(db.schema.vectorCache.id, cachedResponse.id));
-				return jsonParse(content);
+	async function cacheEviction() {
+		if (!cacheMeta.count || cacheMeta.count <= 0) {
+			const currentCount = await vectorDb.count();
+			if (!currentCount) {
+				cacheMeta.count = 0;
+				return;
 			}
+			cacheMeta.count = currentCount;
 		}
-	}
-	return null;
-}
 
-export async function setCachedResponse(
-	response: unknown,
-	inputEmbedding: number[],
-) {
-	db.insert(db.schema.vectorCache)
-		.values({
-			content: jsonStringify(response),
-			lastHitAt: Date.now(),
-		})
-		// .onConflictDoUpdate({
-		// 	target: db.schema.vectorCache.content,
-		// 	set: {
-		// 		lastHitAt: Date.now(),
-		// 	},
-		// })
-		.returning()
-		.then(([cacheEntry]) => {
-			if (cacheEntry) {
-				cacheMeta.count += 1;
-				vectorDb.writeEntry(cacheEntry.id, {
-					vector: inputEmbedding,
-					payload: {},
+		if (
+			cacheMeta.count >= CACHE_EVICTION_TRIGGER_THRESHOLD &&
+			Date.now() - cacheMeta.lastEvictionAt > 5 * MINUTE
+		) {
+			if (CACHE_EVICTION_POLICY === "lfu") {
+				const entriesToEvict = await vectorDb.$client.scroll(vectorDb.key, {
+					order_by: {
+						key: "hits",
+						direction: "asc",
+					},
+					limit: CACHE_ENTRIES_TO_EVICT,
+				});
+				await vectorDb.$client.delete(vectorDb.key, {
+					points: entriesToEvict.points.map((e) => e.id),
 				});
 			}
-		});
+			if (CACHE_EVICTION_POLICY === "lru") {
+				const entriesToEvict = await vectorDb.$client.scroll(vectorDb.key, {
+					order_by: {
+						key: "lastHitAt",
+						direction: "asc",
+					},
+					limit: CACHE_ENTRIES_TO_EVICT,
+				});
+				await vectorDb.$client.delete(vectorDb.key, {
+					points: entriesToEvict.points.map((e) => e.id),
+				});
+			}
+		}
+	}
 
-	cacheEviction();
+	async function getCachedResponse<T>(
+		inputEmbedding: number[],
+		outputSchema: { safeParse: (data: unknown) => { success: boolean } },
+	): Promise<T | null> {
+		const [cachedEntry] = await vectorDb.querySimilar(inputEmbedding, 1);
+
+		if (cachedEntry && cachedEntry.score > 0.998877) {
+			const cachedResponse = cachedEntry.payload;
+
+			if (cachedResponse) {
+				const content = cachedResponse.content;
+				const { success } = outputSchema.safeParse(jsonParse(content));
+
+				if (success) {
+					// register the cache hit but do not need to await it
+					vectorDb.updatePayload(cachedEntry.id, {
+						hits: cachedResponse.hits + 1,
+						lastHitAt: Date.now(),
+					});
+					return jsonParse(content);
+				}
+			}
+		}
+		return null;
+	}
+
+	async function setCachedResponse(
+		response: unknown,
+		inputEmbedding: number[],
+	) {
+		vectorDb.writeEntry(crypto.randomUUID(), {
+			vector: inputEmbedding,
+			payload: {
+				content: jsonStringify(response),
+				hits: 0,
+				lastHitAt: Date.now(),
+			},
+		});
+		cacheEviction();
+	}
+
+	return {
+		getCachedResponse,
+		setCachedResponse,
+	};
 }
