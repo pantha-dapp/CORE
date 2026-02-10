6c16858522173e7c6d7c6643ec30ef8fe52cac85
Spandan Barve <spandan567@gmail.com>
2026-02-10 05:55:48 +0530

    Add attachAi middleware

---
+++ packages/server/api/middleware/attachAi.ts
@@ -0,0 +1,21 @@
+import type { MiddlewareHandler } from "hono";
+import { createAi } from "../../lib/ai";
+import type { AiClient } from "../../lib/ai/client";
+
+export function attachAi(aiClient: AiClient): MiddlewareHandler {
+	return async function attachAi(ctx, next) {
+		const vectorDbClient = ctx.var.vecDbClient;
+		if (!vectorDbClient) {
+			throw new Error(
+				"VectorDbClient not found in context. Make sure attachVecDb middleware is used before attachAi.",
+			);
+		}
+
+		const ai = createAi({
+			aiClient,
+			vectorDbClient,
+		});
+		ctx.set("ai", ai);
+		await next();
+	};
+}
