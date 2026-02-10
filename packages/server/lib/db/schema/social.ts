6ccf17fd7d5a34f6540896b8fdb0e7205443a770
Spandan Barve <spandan567@gmail.com>
2026-02-11 01:25:02 +0530

    Add composite primary key to followings table for follower-following pairs

--- packages/server/lib/db/schema/social.ts
+++ packages/server/lib/db/schema/social.ts
@@ -1,9 +1,13 @@
 import * as t from "drizzle-orm/sqlite-core";
 import { tEvmAddress, timestamps } from "../helpers.base";
 
-export const followings = t.sqliteTable("followings", {
-	follower: tEvmAddress().notNull(),
-	following: tEvmAddress().notNull(),
+export const followings = t.sqliteTable(
+	"followings",
+	{
+		follower: tEvmAddress().notNull(),
+		following: tEvmAddress().notNull(),
 
-	...timestamps,
-});
+		...timestamps,
+	},
+	(table) => [t.primaryKey({ columns: [table.follower, table.following] })],
+);
