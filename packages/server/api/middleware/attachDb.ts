b72258a561144aeb4451a2723c0cb6f439c2a610
Spandan Barve <spandan567@gmail.com>
2026-02-10 05:54:26 +0530

    Update attachDb middleware

--- packages/server/api/middleware/attachDb.ts
+++ packages/server/api/middleware/attachDb.ts
@@ -1,8 +1,12 @@
 import type { MiddlewareHandler } from "hono";
 import { createDb } from "../../lib/db";
+import type { VectorDbClient } from "../../lib/db/vec/client";
 
-export function attachDb(filename: string): MiddlewareHandler {
-	const db = createDb(filename);
+export function attachDb(
+	filename: string,
+	vectorDbClient: VectorDbClient,
+): MiddlewareHandler {
+	const db = createDb(filename, vectorDbClient);
 
 	return async function attachDb(ctx, next) {
 		ctx.set("db", db);
