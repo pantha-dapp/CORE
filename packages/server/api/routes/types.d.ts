2574cb8745b8cbbdbf8708075ea7d97d88b4cf9e
Spandan Barve <spandan567@gmail.com>
2026-02-10 05:54:48 +0530

    Update routes types

--- packages/server/api/routes/types.d.ts
+++ packages/server/api/routes/types.d.ts
@@ -1,7 +1,11 @@
+import type { Ai } from "../../lib/ai";
 import type { Db } from "../../lib/db";
+import type { VectorDbClient } from "../../lib/db/vec/client";
 
 export type RouterEnv = {
 	Variables: {
 		db: Db;
+		ai: Ai;
+		vecDbClient: VectorDbClient;
 	};
 };
