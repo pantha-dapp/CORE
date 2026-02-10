5f143f61f713b4b53689727f0950ef62d3605b62
Spandan Barve <spandan567@gmail.com>
2026-02-10 05:54:41 +0530

    Update server router

--- packages/server/api/routes/router.ts
+++ packages/server/api/routes/router.ts
@@ -1,12 +1,24 @@
+import { QdrantClient } from "@qdrant/js-client-rest";
 import { Hono } from "hono";
 import env from "../../env";
+import { aiAdapter } from "../../lib/ai/engine";
+import { attachAi } from "../middleware/attachAi";
 import { attachDb } from "../middleware/attachDb";
 import auth from "./auth";
 import course from "./course";
 import jobs from "./jobs";
 
 export const apiRouter = new Hono()
-	.use(attachDb(env.SQLITE_FILE_PATH))
+	.use(
+		attachDb(
+			env.SQLITE_FILE_PATH,
+			new QdrantClient({
+				host: env.QDRANT_HOST,
+				port: parseInt(env.QDRANT_PORT, 10),
+			}),
+		),
+	)
+	.use(attachAi(aiAdapter))
 	.get("/runtime", (ctx) => {
 		const runtime = {
 			uptime: process.uptime(),
