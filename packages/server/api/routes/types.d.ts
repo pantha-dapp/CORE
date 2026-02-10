11c248a323311c84884c2568c32130437f422bc2
Spandan Barve <spandan567@gmail.com>
2026-02-10 23:21:44 +0530

    Update routes types

--- packages/server/api/routes/types.d.ts
+++ packages/server/api/routes/types.d.ts
@@ -1,8 +1,10 @@
 import type { Ai } from "../../lib/ai";
 import type { Db } from "../../lib/db";
+import type { EventBus } from "../../lib/events/bus";
 
 export type RouterEnv = {
 	Variables: {
+		eventBus: EventBus;
 		db: Db;
 		ai: Ai;
 	};
