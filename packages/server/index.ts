c00b970a4df41e2be86a106ea72d4b3680a4e065
Spandan Barve <spandan567@gmail.com>
2026-02-11 00:50:46 +0530

    Update server index

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -4,7 +4,29 @@ import env, { ensureEnv } from "./env";
 
 ensureEnv();
 
+import { QdrantClient } from "@qdrant/js-client-rest";
+import { RedisClient } from "bun";
+import { attachAi } from "./api/middleware/attachAi";
+import { attachDb } from "./api/middleware/attachDb";
+import { attachEventBus } from "./api/middleware/attachEventBus";
+import { aiAdapter } from "./lib/ai/engine";
+import { InMemoryEventBus } from "./lib/events/bus";
 import { app } from "./server";
+
+app
+	.use(
+		attachDb(
+			env.SQLITE_FILE_PATH,
+			new QdrantClient({
+				host: env.QDRANT_HOST,
+				port: parseInt(env.QDRANT_PORT, 10),
+				checkCompatibility: false,
+			}),
+			new RedisClient(),
+		),
+	)
+	.use(attachAi(aiAdapter))
+	.use(attachEventBus(new InMemoryEventBus()));
 
 //@ts-expect-error
 BigInt.prototype.toJSON = function () {
