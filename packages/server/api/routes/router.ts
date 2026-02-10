9cd892238d73ac35dfcbe2840fc32f4bc4d2f486
Spandan Barve <spandan567@gmail.com>
2026-02-11 00:50:24 +0530

    Update server router

--- packages/server/api/routes/router.ts
+++ packages/server/api/routes/router.ts
@@ -1,12 +1,4 @@
-import { QdrantClient } from "@qdrant/js-client-rest";
-import { RedisClient } from "bun";
 import { Hono } from "hono";
-import env from "../../env";
-import { aiAdapter } from "../../lib/ai/engine";
-import { InMemoryEventBus } from "../../lib/events/bus";
-import { attachAi } from "../middleware/attachAi";
-import { attachDb } from "../middleware/attachDb";
-import { attachEventBus } from "../middleware/attachEventBus";
 import auth from "./auth";
 import course from "./course";
 import jobs from "./jobs";
@@ -14,19 +6,6 @@ import type { RouterEnv } from "./types";
 import users from "./users";
 
 export const apiRouter = new Hono<RouterEnv>()
-
-	.use(
-		attachDb(
-			env.SQLITE_FILE_PATH,
-			new QdrantClient({
-				host: env.QDRANT_HOST,
-				port: parseInt(env.QDRANT_PORT, 10),
-			}),
-			new RedisClient(),
-		),
-	)
-	.use(attachAi(aiAdapter))
-	.use(attachEventBus(new InMemoryEventBus()))
 
 	.get("/runtime", (ctx) => {
 		const runtime = {
