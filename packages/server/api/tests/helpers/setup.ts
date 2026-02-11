f0a8cef2de1d5d24052bfc6451e9999fed389f0a
Spandan Barve <spandan567@gmail.com>
2026-02-12 03:30:59 +0530

    Add test setup helper file

---
+++ packages/server/api/tests/helpers/setup.ts
@@ -0,0 +1,108 @@
+import { afterAll, beforeAll } from "bun:test";
+import { QdrantClient } from "@qdrant/js-client-rest";
+import { RedisClient } from "bun";
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
+import { Hono } from "hono";
+import { hc } from "hono/client";
+import {
+	GenericContainer,
+	type StartedTestContainer,
+	Wait,
+} from "testcontainers";
+import { createWalletClient, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { hardhat } from "viem/chains";
+import { createAi } from "../../../lib/ai";
+import { createDb } from "../../../lib/db";
+import { InMemoryEventBus } from "../../../lib/events/bus";
+import { apiRouter } from "../../routes/router";
+import type { RouterEnv } from "../../routes/types";
+import { createAuthenticatedApi } from "./apiAuth";
+import { testGlobals } from "./globals";
+import { testAiAdapter } from "./testAiAdapter";
+
+// let api0 = hc<typeof apiRouter>("http://error");
+// let api1 = hc<typeof apiRouter>("http://error");
+// let api2 = hc<typeof apiRouter>("http://error");
+
+export const userWallet1 = createWalletClient({
+	account: privateKeyToAccount(
+		"0x7b9a333cc8f8558f744fd43bae30c1cf9e33e3f5b1a9e8ca3402edec728dde84",
+	),
+	transport: http(),
+	chain: hardhat,
+});
+export const userWallet2 = createWalletClient({
+	account: privateKeyToAccount(
+		"0x7b9a333cc8f8558f744fd43bae30c1cf9e33e3f5b1a9e8ca3402edec728dde75",
+	),
+	transport: http(),
+	chain: hardhat,
+});
+let qdrant: StartedTestContainer;
+let redis: StartedTestContainer;
+
+beforeAll(
+	async () => {
+		qdrant = await new GenericContainer("qdrant/qdrant")
+			.withExposedPorts(6333)
+			.withWaitStrategy(Wait.forHttp("/collections", 6333))
+			.start();
+
+		const testVecDb = new QdrantClient({
+			host: "localhost",
+			port: qdrant.getMappedPort(6333),
+			checkCompatibility: false,
+		});
+
+		redis = await new GenericContainer("redis:7-alpine")
+			.withExposedPorts(6379)
+			.withWaitStrategy(Wait.forLogMessage("* Ready to accept connections"))
+			.start();
+		const testRedis = new RedisClient(
+			`redis://localhost:${redis.getMappedPort(6379)}`,
+			{ maxRetries: 3 },
+		);
+
+		const testDb = createDb(":memory:", {
+			vectorDbClient: testVecDb,
+			redisClient: testRedis,
+		});
+
+		migrate(testDb.$db, {
+			migrationsFolder: "./drizzle",
+		});
+
+		const testApi = new Hono<RouterEnv>()
+			.use("*", async (ctx, next) => {
+				ctx.set("db", testDb);
+				ctx.set(
+					"ai",
+					createAi({
+						aiClient: testAiAdapter,
+						vectorDbClient: testVecDb,
+					}),
+				);
+				ctx.set("eventBus", new InMemoryEventBus());
+
+				await next();
+			})
+			.route("/", apiRouter);
+
+		testGlobals.api0 = hc<typeof apiRouter>("http://localhost", {
+			fetch: async (input: string, init: RequestInit | undefined) => {
+				const request = new Request(input, init);
+				return testApi.fetch(request);
+			},
+		});
+
+		testGlobals.api1 = await createAuthenticatedApi(testApi, userWallet1);
+		testGlobals.api2 = await createAuthenticatedApi(testApi, userWallet2);
+	},
+	{ timeout: 20_000 },
+);
+
+afterAll(async () => {
+	await qdrant.stop();
+	await redis.stop();
+});
