947424af0a2e413157927b4fca9b76d38850069c
Spandan Barve <spandan567@gmail.com>
2026-02-12 03:30:36 +0530

    Add courses chapters route

---
+++ packages/server/api/routes/courses/chapters/index.ts
@@ -0,0 +1,124 @@
+import { and, eq } from "drizzle-orm";
+import { Hono } from "hono";
+import z from "zod";
+import { prepareChapter } from "../../../../lib/utils/chapters";
+import { respond } from "../../../../lib/utils/respond";
+import { authenticated } from "../../../middleware/auth";
+import { validator } from "../../../middleware/validator";
+import type { RouterEnv } from "../../types";
+import session from "./session";
+
+export default new Hono<RouterEnv>()
+
+	.route("/session", session)
+
+	.get(
+		"/:id",
+		authenticated,
+		validator(
+			"param",
+			z.object({
+				id: z.string(),
+			}),
+		),
+		async (ctx) => {
+			const { db } = ctx.var;
+			const { id } = ctx.req.valid("param");
+
+			const chapter = await db.chapterById({ chapterId: id });
+
+			if (!chapter) {
+				return respond.err(ctx, "Chapter not found.", 404);
+			}
+
+			return respond.ok(ctx, { chapter }, "Chapter fetched successfully.", 200);
+		},
+	)
+
+	.get(
+		"/:id/pages",
+		authenticated,
+		validator(
+			"param",
+			z.object({
+				id: z.string(),
+			}),
+		),
+		async (ctx) => {
+			const { db, ai } = ctx.var;
+			const { id } = ctx.req.valid("param");
+
+			const chapter = await db.chapterById({ chapterId: id });
+			if (!chapter) {
+				return respond.err(ctx, "Chapter not found.", 404);
+			}
+
+			const pages = await db.chapterPagesById({ chapterId: id });
+			if (!pages || pages.length === 0) {
+				prepareChapter(chapter.id, { db, ai });
+				return respond.err(
+					ctx,
+					"Chapter pages unavailable at the moment.",
+					503,
+					{ "Retry-After": "30" },
+				);
+			}
+
+			const [nextChapter] = await db
+				.select()
+				.from(db.schema.courseChapters)
+				.where(
+					and(
+						eq(db.schema.courseChapters.courseId, chapter.courseId),
+						eq(db.schema.courseChapters.order, chapter.order + 1),
+					),
+				);
+			nextChapter && prepareChapter(nextChapter.id, { db, ai });
+
+			//safepages generaton
+			pages.forEach((p) => {
+				const { type, content } = p.content;
+				switch (type) {
+					case "example_uses":
+						return;
+					case "fill_in_the_blanks": {
+						content.wrongOptions = content.wrongOptions.concat(content.answers);
+						content.answers = [];
+						return;
+					}
+					case "identify_object_from_images": {
+						content.correctImageIndex = -1;
+						return;
+					}
+					case "identify_shown_object_in_image": {
+						content.correctOptionIndex = -1;
+						return;
+					}
+					case "matching": {
+						// sanitization needed but lets do this later
+						return;
+					}
+					case "quiz": {
+						content.correctOptionIndex = -1;
+						return;
+					}
+					case "teach_and_explain_content": {
+						return;
+					}
+					case "true_false": {
+						content.isTrue = false;
+						return;
+					}
+					default:
+						return;
+				}
+			});
+
+			return respond.ok(
+				ctx,
+				{ pages },
+				"Chapter pages fetched successfully.",
+				200,
+			);
+		},
+	);
