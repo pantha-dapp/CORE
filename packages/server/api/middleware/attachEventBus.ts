99672071112c7630b3d1526bc0f477b2958e9394
Spandan Barve <spandan567@gmail.com>
2026-02-14 13:10:22 +0530

    Remove attachEventBus middleware

--- packages/server/api/middleware/attachEventBus.ts
+++
@@ -1,9 +0,0 @@
-import type { MiddlewareHandler } from "hono";
-import type { EventBus } from "../../lib/events/bus";
-
-export function attachEventBus(eventBus: EventBus): MiddlewareHandler {
-	return async function attachEventBus(ctx, next) {
-		ctx.set("eventBus", eventBus);
-		await next();
-	};
-}
