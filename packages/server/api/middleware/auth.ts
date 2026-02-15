72cb6b94fbbc5c11ae3b4e02737d1215f51a6c7e
Spandan Barve <spandan567@gmail.com>
2026-02-15 06:11:25 +0530

    Update auth middleware

--- packages/server/api/middleware/auth.ts
+++ packages/server/api/middleware/auth.ts
@@ -1,5 +1,6 @@
+import { eq } from "drizzle-orm";
 import { createMiddleware } from "hono/factory";
-import type { Address } from "viem";
+import { type Address, isAddress } from "viem";
 import { verifyJwt } from "../../lib/utils/jwt";
 import { respond } from "../../lib/utils/respond";
 import type { RouterEnv } from "../routes/types";
@@ -10,6 +11,8 @@ export const authenticated = createMiddleware<{
 	} & RouterEnv["Variables"];
 }>(async (ctx, next) => {
 	const { db, eventBus } = ctx.var.appState;
+	const cache = db.redis;
+
 	if (!db) {
 		throw new Error(
 			"Database instance not found in context, instantiate attachDb middleware before authenticated middleware",
@@ -29,23 +32,55 @@ export const authenticated = createMiddleware<{
 	} catch (_) {
 		return respond.err(ctx, "Invalid or expired token", 401);
 	}
-
 	if (!payload || !payload.sub) {
 		return respond.err(ctx, "Invalid or expired token", 401);
 	}
 
-	ctx.set("userWallet", payload.sub);
-	await next();
+	const cachedSessionWallet = await cache.get(
+		`user_session:wallet:${payload.sub}`,
+	);
+	const cachedSessionExpiry = await cache.get(
+		`user_session:expiry:${payload.sub}`,
+	);
+	if (cachedSessionWallet && cachedSessionExpiry) {
+		if (
+			parseInt(cachedSessionExpiry, 10) > Date.now() &&
+			isAddress(cachedSessionWallet)
+		) {
+			ctx.set("userWallet", cachedSessionWallet);
+		} else {
+			await cache.del(`user_session:wallet:${payload.sub}`);
+			await cache.del(`user_session:expiry:${payload.sub}`);
+			return respond.err(ctx, "Invalid or expired session", 401);
+		}
+	} else {
+		const [session] = await db
+			.select()
+			.from(db.schema.userSessions)
+			.where(eq(db.schema.userSessions.id, payload.sub))
+			.limit(1);
 
-	// db.insert(db.schema.users)
-	// 	.values({
-	// 		walletAddress: payload.sub,
-	// 		lastActiveAt: new Date(),
-	// 	})
-	// 	.onConflictDoUpdate({
-	// 		target: db.schema.users.walletAddress,
-	// 		set: {
-	// 			lastActiveAt: new Date(),
-	// 		},
-	// 	});
+		if (!session) {
+			return respond.err(ctx, "Session not found", 401);
+		}
+
+		ctx.set("userWallet", session.userWallet);
+
+		cache.setex(
+			`user_session:wallet:${payload.sub}`,
+			payload.exp,
+			session.userWallet,
+		);
+		cache.setex(
+			`user_session:expiry:${payload.sub}`,
+			payload.exp,
+			payload.exp.toString(),
+		);
+	}
+
+	if (!isAddress(ctx.var.userWallet)) {
+		return respond.err(ctx, "Invalid wallet address in token", 401);
+	}
+
+	await next();
 });
