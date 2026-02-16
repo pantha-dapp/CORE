c4838559e2c7231184c328945459897d21da841b
Spandan Barve <spandan567@gmail.com>
2026-02-16 23:57:13 +0530

    Add network error classes

---
+++ packages/server/lib/errors/network.ts
@@ -0,0 +1,19 @@
+import { AppError } from "./app";
+
+export class ServiceUnavailableError extends AppError {
+	constructor(message = "Service unavailable") {
+		super(message, "SERVICE_UNAVAILABLE", 503);
+	}
+}
+
+export class TimeoutError extends AppError {
+	constructor(message = "Operation timed out") {
+		super(message, "TIMEOUT", 504);
+	}
+}
+
+export class NotImplementedError extends AppError {
+	constructor(message = "Feature not implemented") {
+		super(message, "NOT_IMPLEMENTED", 501);
+	}
+}
