0e5227d3897c1ebfd586bad877d574d84b3c5120
Spandan Barve <spandan567@gmail.com>
2026-03-11 02:23:03 +0530

    feat: implement S3ObjectStorage class to handle object storage operations

---
+++ packages/server/lib/objectStorage/s3.ts
@@ -0,0 +1,66 @@
+import { S3Client } from "bun";
+import {
+	type ObjectStorageAdapter,
+	ObjectStorageResourceDefs,
+	type ObjectStorageResourceKey,
+} from ".";
+
+type S3ObjectStorageConfig = {
+	rootDir: string[];
+	accessKeyId: string;
+	secretAccessKey: string;
+	bucket: string;
+	endpoint: string;
+};
+export class S3ObjectStorage implements ObjectStorageAdapter {
+	private _config: S3ObjectStorageConfig;
+
+	constructor(config: S3ObjectStorageConfig) {
+		this._config = config;
+	}
+
+	get $s3() {
+		return new S3Client({
+			accessKeyId: this._config.accessKeyId,
+			secretAccessKey: this._config.secretAccessKey,
+			endpoint: this._config.endpoint,
+			bucket: this._config.bucket,
+		});
+	}
+
+	file(path: string[]) {
+		return this.$s3.file([this._config.rootDir, ...path].join("/"));
+	}
+
+	async upload(
+		key: ObjectStorageResourceKey,
+		args: { path: string[]; data: Buffer },
+	) {
+		const { path, data } = args;
+		const def = ObjectStorageResourceDefs.find((def) => def.key === key);
+		if (!def) {
+			throw new Error(`Invalid object storage resource key: ${key}`);
+		}
+		if (data.length > def.maxSizeBytes) {
+			throw new Error(
+				`File size exceeds maximum of ${def.maxSizeBytes} bytes for resource type ${key}`,
+			);
+		}
+		const pathStr = [def.key, ...path].join("/");
+
+		const file = this.$s3.file(pathStr);
+
+		await file.write(data);
+
+		const url = file.presign();
+
+		return { url };
+	}
+
+	async delete(args: { path: string[] }) {
+		const { path } = args;
+		const pathStr = path.join("/");
+		const file = this.$s3.file(pathStr);
+		await file.delete();
+	}
+}
