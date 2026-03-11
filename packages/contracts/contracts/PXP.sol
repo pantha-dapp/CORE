fcd52193be0678e5e4f77aec116e8ab02d2e109f
Spandan Barve <spandan567@gmail.com>
2026-03-12 04:38:21 +0530

    feat(PXP): implement ownership control for minting function

--- packages/contracts/contracts/PXP.sol
+++ packages/contracts/contracts/PXP.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PXP is ERC20 {
-    constructor() ERC20("PanthaXP", "PXP") {}
+contract PXP is ERC20, Ownable {
+    constructor() ERC20("PanthaXP", "PXP") Ownable(msg.sender) {}
 
-    function mint(address to, uint256 amount) external {
+    function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 }
