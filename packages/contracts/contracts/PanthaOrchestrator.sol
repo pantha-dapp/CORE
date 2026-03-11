e7e3926fed4804bfb88b4cabc39b991eac5e92cb
Spandan Barve <spandan567@gmail.com>
2026-03-12 00:34:23 +0530

    contracts: add PanthaOrchestrator.sol

---
+++ packages/contracts/contracts/PanthaOrchestrator.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract PanthaOrchestrator {
+    IERC20 public panthaToken;
+}
