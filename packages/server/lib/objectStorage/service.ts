5c53e12b638d8b302806da02edca53609d004318
Spandan Barve <spandan567@gmail.com>
2026-03-12 00:34:23 +0530

    objectStorage: implement service methods

--- packages/server/lib/objectStorage/service.ts
+++ packages/server/lib/objectStorage/service.ts
@@ -1,12 +1,18 @@
+import type { ObjectStorageResourceKey } from ".";
 import { S3ObjectStorage } from "./s3";
 import { FilecoinSynapseObjectStorage } from "./synapse";
 
 export interface IObjectStorageService {
-	upload: (args: { path: string[]; data: Buffer }) => {
+	upload: (args: {
+		path: [ObjectStorageResourceKey, ...string[]];
+		data: Buffer;
+	}) => {
 		hotStorage: Promise<{ url: string }>;
 		persistentStorage: Promise<{ url: string }>;
 	};
-	unloadHot: (args: { path: string[] }) => Promise<void>;
+	unloadHot: (args: {
+		path: [ObjectStorageResourceKey, ...string[]];
+	}) => Promise<void>;
 }
 
 export class ObjectStorageService implements IObjectStorageService {
@@ -21,17 +27,20 @@ export class ObjectStorageService implements IObjectStorageService {
 		this.synapse = new FilecoinSynapseObjectStorage(config.synapse);
 	}
 
-	upload(args: { path: string[]; data: Buffer }) {
+	upload(args: {
+		path: [ObjectStorageResourceKey, ...string[]];
+		data: Buffer;
+	}) {
 		const { path, data } = args;
 
-		const hotStorageResultPromise = this.s3.upload("hot_storage_key", {
-			path,
+		const hotStorageResultPromise = this.s3.upload(path[0], {
+			path: path.slice(1),
 			data,
 		});
-		const persistentStorageResultPromise = this.synapse.upload(
-			"persistent_storage_key",
-			{ path, data },
-		);
+		const persistentStorageResultPromise = this.synapse.upload(path[0], {
+			path: path.slice(1),
+			data,
+		});
 
 		return {
 			hotStorage: hotStorageResultPromise,
@@ -39,7 +48,7 @@ export class ObjectStorageService implements IObjectStorageService {
 		};
 	}
 
-	async unloadHot(args: { path: string[] }) {
+	async unloadHot(args: { path: [ObjectStorageResourceKey, ...string[]] }) {
 		await this.s3.delete({ path: args.path });
 	}
 }
