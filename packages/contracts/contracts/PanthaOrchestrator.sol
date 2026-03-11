2b674d9d89e6b69bef0ef40fdae07b9c8e78a9d6
Spandan Barve <spandan567@gmail.com>
2026-03-12 04:38:18 +0530

    feat(rewards): implement rewards distribution functionality in PanthaOrchestrator

--- packages/contracts/contracts/PanthaOrchestrator.sol
+++ packages/contracts/contracts/PanthaOrchestrator.sol
@@ -2,7 +2,35 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./PXP.sol";
 
 contract PanthaOrchestrator {
-    IERC20 public panthaToken;
+    IERC20 public pxp;
+    address public server;
+
+    event RewardsDistributed(
+        address indexed recipient,
+        uint256 amount,
+        bytes8 reason,
+        bytes8 reasonResourceIdentifier
+    );
+
+    modifier onlyServer() {
+        require(msg.sender == server, "Only server can call this function");
+        _;
+    }
+
+    constructor() {
+        pxp = new PXP();
+        server = msg.sender;
+    }
+
+    function distributeRewards(
+        address recipient,
+        uint256 amount,
+        bytes8 reason
+    ) external onlyServer {
+        PXP(address(pxp)).mint(recipient, amount);
+        emit RewardsDistributed(recipient, amount, reason);
+    }
 }
