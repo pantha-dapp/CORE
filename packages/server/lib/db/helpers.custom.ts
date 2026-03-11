61d435004d9489df8abb6b38763e2b3bc46bcd83
Spandan Barve <spandan567@gmail.com>
2026-03-11 21:51:09 +0530

    db: add custom helpers

--- packages/server/lib/db/helpers.custom.ts
+++ packages/server/lib/db/helpers.custom.ts
@@ -1,6 +1,6 @@
 import { jsonParse, jsonStringify } from "@pantha/shared";
 import { customType } from "drizzle-orm/sqlite-core";
-import type z from "zod";
+import z from "zod";
 import { generateChapterPageOutputTypedSchema } from "../ai/tasks/generateChapterPage.schemas";
 
 export const tPageContent = customType<{
@@ -15,5 +15,22 @@ export const tPageContent = customType<{
 	},
 	fromDriver(value) {
 		return generateChapterPageOutputTypedSchema.parse(jsonParse(value));
+	},
+});
+
+export const tImageParts = customType<{
+	data: { url: string | null; prompt: string };
+	driverData: string;
+}>({
+	dataType() {
+		return "text";
+	},
+	toDriver(value) {
+		return jsonStringify(value);
+	},
+	fromDriver(value) {
+		return z
+			.object({ url: z.string().nullable(), prompt: z.string() })
+			.parse(jsonParse(value));
 	},
 });
