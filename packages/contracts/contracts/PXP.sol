9d2dec79761b2f00d44e8200a4c045576bc23e4d
Spandan Barve <spandan567@gmail.com>
2026-03-12 06:24:19 +0530

    make pxp non transferable

--- packages/contracts/contracts/PXP.sol
+++ packages/contracts/contracts/PXP.sol
@@ -5,9 +5,30 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PXP is ERC20, Ownable {
+    error NonTransferable();
+
     constructor() ERC20("PanthaXP", "PXP") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
+    }
+
+    /// @notice Transfers are disabled - PXP is soulbound
+    function transfer(address, uint256) public pure override returns (bool) {
+        revert NonTransferable();
+    }
+
+    /// @notice Transfers are disabled - PXP is soulbound
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) public pure override returns (bool) {
+        revert NonTransferable();
+    }
+
+    /// @notice Approvals are disabled - PXP is soulbound
+    function approve(address, uint256) public pure override returns (bool) {
+        revert NonTransferable();
     }
 }
