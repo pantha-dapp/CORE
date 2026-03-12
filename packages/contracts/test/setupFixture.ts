d2699f61ebe6e31731a32880a611726c9bbe17e9
Spandan Barve <spandan567@gmail.com>
2026-03-13 01:31:23 +0530

    Add test fixture setup for contract integration tests

---
+++ packages/contracts/test/setupFixture.ts
@@ -0,0 +1,25 @@
+import { network } from "hardhat";
+
+export async function setupFixture() {
+	const { viem } = await network.connect();
+	const [owner, user] = await viem.getWalletClients();
+	const publicClient = await viem.getPublicClient();
+
+	const token = await viem.deployContract("PanthaToken", [
+		BigInt(100 * 10 ** 18),
+	]);
+	const orchestrator = await viem.deployContract("PanthaOrchestrator", [
+		token.address,
+	]);
+	const pxpAddress = await orchestrator.read.pxp();
+	const pxp = await viem.getContractAt("PXP", pxpAddress);
+	const ca = await orchestrator.read.certificationAuthority();
+
+	return {
+		viem,
+		token,
+		owner,
+		user,
+		publicClient,
+	};
+}
