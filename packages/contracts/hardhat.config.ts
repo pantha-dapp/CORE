472e5cdf4b420b58713da1a04dfa0d9ac986bce8
Spandan Barve <spandan567@gmail.com>
2026-03-13 08:06:18 +0530

    style: format hardhat.config.ts for improved readability

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -3,46 +3,46 @@ import hardhatViem from "@nomicfoundation/hardhat-viem";
 import { configVariable, defineConfig } from "hardhat/config";
 
 export default defineConfig({
-    plugins: [hardhatToolboxViemPlugin, hardhatViem],
-    solidity: {
-        profiles: {
-            default: {
-                version: "0.8.28",
-            },
-            production: {
-                version: "0.8.28",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 200,
-                    },
-                },
-            },
-        },
-    },
-    paths: {
-        sources: "src",
-    },
-    networks: {
-        hardhatMainnet: {
-            type: "edr-simulated",
-            chainType: "l1",
-        },
-        hardhatOp: {
-            type: "edr-simulated",
-            chainType: "op",
-        },
-        sepolia: {
-            type: "http",
-            chainType: "l1",
-            url: configVariable("SEPOLIA_RPC_URL"),
-            accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
-        },
-        flowEvmTestnet: {
-            type: "http",
-            chainId: 545,
-            url: "https://testnet.evm.nodes.onflow.org",
-            accounts: [configVariable("FLOW_TESTNET_PRIVATE_KEY")],
-        }
-    },
+	plugins: [hardhatToolboxViemPlugin, hardhatViem],
+	solidity: {
+		profiles: {
+			default: {
+				version: "0.8.28",
+			},
+			production: {
+				version: "0.8.28",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+		},
+	},
+	paths: {
+		sources: "src",
+	},
+	networks: {
+		hardhatMainnet: {
+			type: "edr-simulated",
+			chainType: "l1",
+		},
+		hardhatOp: {
+			type: "edr-simulated",
+			chainType: "op",
+		},
+		sepolia: {
+			type: "http",
+			chainType: "l1",
+			url: configVariable("SEPOLIA_RPC_URL"),
+			accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+		},
+		flowEvmTestnet: {
+			type: "http",
+			chainId: 545,
+			url: "https://testnet.evm.nodes.onflow.org",
+			accounts: [configVariable("FLOW_TESTNET_PRIVATE_KEY")],
+		},
+	},
 });
