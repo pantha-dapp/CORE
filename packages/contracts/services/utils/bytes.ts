2cb41bfa6931ac9c7738b5b8deb4c8b8c95c4e73
Spandan Barve <spandan567@gmail.com>
2026-03-14 00:56:58 +0530

    Refactor identifierB8 function to return a Hex type and use sliceHex for improved output

--- packages/contracts/services/utils/bytes.ts
+++ packages/contracts/services/utils/bytes.ts
@@ -1,6 +1,7 @@
-import { keccak256, pad, toHex } from "viem";
+import { type Hex, keccak256, pad, sliceHex, toHex } from "viem";
 
 export const bytes8 = (input: Parameters<typeof toHex>[0]) =>
 	pad(toHex(input), { size: 8 });
 
-export const identifierB8 = (input: string) => bytes8(keccak256(toHex(input)));
+export const identifierB8 = (input: string): Hex =>
+	sliceHex(keccak256(toHex(input)), 0, 8);
