9a9896af5e8cc2b0300dd1bc67e2b7fe83d3812d
Spandan Barve <spandan567@gmail.com>
2026-03-14 08:08:04 +0530

    Refactor AiClient interface to use JsonFunction type for json method

--- packages/server/lib/ai/client.ts
+++ packages/server/lib/ai/client.ts
@@ -1,18 +1,23 @@
 import type z from "zod";
 import type { LanguageCode } from "../../data/languages";
 
+type JsonFunction = <T, R>(args: {
+	prompt: string;
+	input: T;
+	outputSchema: z.ZodType<R>;
+	systemPrompts?: string[];
+}) => Promise<R>;
+
 export interface AiClient {
 	llm: {
 		text: (args: {
 			prompt: string;
 			systemPrompts?: string[];
 		}) => Promise<string>;
-		json: <T, R>(args: {
-			prompt: string;
-			input: T;
-			outputSchema: z.ZodType<R>;
-			systemPrompts?: string[];
-		}) => Promise<R>;
+		json: JsonFunction;
+		heal: (
+			args: Parameters<JsonFunction>[0] & { failedGeneration: string },
+		) => Promise<string>;
 	};
 	embedding: {
 		text: (input: string) => Promise<number[]>;
