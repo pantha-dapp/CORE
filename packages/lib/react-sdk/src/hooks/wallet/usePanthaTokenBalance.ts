7fe579c9252fe08d0240e23e81eb531b1f9dbde2
Spandan Barve <spandan567@gmail.com>
2026-03-14 06:59:01 +0530

    Add useFaucet hook for fetching PanthaToken balance in wallet context

---
+++ packages/lib/react-sdk/src/hooks/wallet/usePanthaTokenBalance.ts
@@ -0,0 +1,18 @@
+import { useMutation } from "@tanstack/react-query";
+import { usePanthaContext } from "../../context/PanthaProvider";
+
+export function useFaucet() {
+	const { contracts, wallet } = usePanthaContext();
+
+	return useMutation({
+		mutationFn: async () => {
+			if (!contracts || !wallet) throw new Error("not connected");
+
+			const balance = await contracts.PanthaToken.read.balanceOf([
+				wallet.account.address,
+			]);
+
+			return balance;
+		},
+	});
+}
