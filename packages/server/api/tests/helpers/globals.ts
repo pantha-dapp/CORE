160837b7e9c10f40698e905a078365d8a9903fb8
Spandan Barve <spandan567@gmail.com>
2026-03-16 23:53:16 +0530

    Add reauthenticate function to testGlobals for improved test setup

--- packages/server/api/tests/helpers/globals.ts
+++ packages/server/api/tests/helpers/globals.ts
@@ -6,4 +6,5 @@ export const testGlobals: {
 	api0: ReturnType<typeof hc<typeof apiRouter>>;
 	api1: ReturnType<typeof hc<typeof apiRouter>>;
 	api2: ReturnType<typeof hc<typeof apiRouter>>;
+	reauthenticate: () => Promise<void>;
 } = {};
