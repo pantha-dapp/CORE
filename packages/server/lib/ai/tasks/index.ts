5c42a9a2e00daa88098f2c212b9e3d4a897fa9b1
Abhishek Chaudhary <abhishek97208@gmail.com>
2026-03-17 03:29:34 +0530

    Remove unused generateChapterPagesOverview import from tasks index

--- packages/server/lib/ai/tasks/index.ts
+++ packages/server/lib/ai/tasks/index.ts
@@ -2,7 +2,6 @@ import clarificationQuestionGenerator from "./clarificationQuestionGenerator";
 import courseSelectionEvaluator from "./courseSelectionEvaluator";
 import generateAnswerExplanation from "./generateAnswerExplanation";
 import generateChapterPages from "./generateChapterPages.legacy";
-import generateChapterPagesOverview from "./generateChapterPagesOverview";
 import generateIdealCourseDescriptor from "./generateIdealCourseDescriptor";
 import generateNewCourseSkeleton from "./generateNewCourseSkeleton";
 import intentClarification from "./intentClarification";
@@ -13,7 +12,6 @@ const tasks = {
 	courseSelectionEvaluator,
 	generateAnswerExplanation,
 	generateChapterPages,
-	generateChapterPagesOverview,
 	generateIdealCourseDescriptor,
 	generateNewCourseSkeleton,
 	intentClarification,
