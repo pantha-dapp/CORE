81eb59450a21e525acc99ca52c30a810abe66cbe
Spandan Barve <spandan567@gmail.com>
2026-03-17 09:49:13 +0530

    fix buyWithPermit to use permit instead of transfer

--- packages/contracts/src/PanthaShop.sol
+++ packages/contracts/src/PanthaShop.sol
@@ -2,18 +2,24 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IPanthaOrchestrator.sol";
+import "./interfaces/IERC20PermitToken.sol";
 
 contract PanthaShop {
     IERC20PermitToken public immutable token;
     IPanthaOrchestrator public immutable orchestrator;
 
+    event ItemPurchased(
+        address indexed buyer,
+        bytes32 indexed purchaseId,
+        uint256 amount
+    );
+
     constructor() {
         orchestrator = IPanthaOrchestrator(msg.sender); // the orchestrator deploys  shop
-        token = orchestrator.panthaToken();
+        token = IERC20PermitToken(orchestrator.panthaToken());
     }
 
     function buyWithPermit(
@@ -23,15 +29,11 @@ contract PanthaShop {
         uint8 v,
         bytes32 r,
         bytes32 s,
-        bytes32 itemId // or uint256
+        bytes32 purchaseId
     ) external {
-        // 1. Approve via signature
         token.permit(owner, address(this), value, deadline, v, r, s);
+        token.transferFrom(owner, orchestrator.treasury(), value);
 
-        // 2. Transfer tokens
-        token.transferFrom(owner, treasury, value);
-
-        // 3. Execute logic
-        _deliverItem(owner, itemId, value);
+        emit ItemPurchased(owner, purchaseId, value);
     }
 }
