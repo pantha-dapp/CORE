deaf8bc2afde523865e34f3cb2baa82154866a32
Spandan Barve <spandan567@gmail.com>
2026-03-18 02:40:19 +0530

    update supportedChains to use mainnet in privy config

--- packages/web/src/shared/config/privy.ts
+++ packages/web/src/shared/config/privy.ts
@@ -1,5 +1,5 @@
 import type { PrivyClientConfig } from "@privy-io/react-auth";
-import { flowTestnet } from "viem/chains";
+import { mainnet } from "viem/chains";
 
 export const privyConfig: PrivyClientConfig = {
 	appearance: {
@@ -13,5 +13,5 @@ export const privyConfig: PrivyClientConfig = {
 		showWalletUIs: false,
 	},
 	loginMethods: ["email", "google", "apple", "farcaster"],
-	supportedChains: [flowTestnet],
+	supportedChains: [mainnet],
 };
