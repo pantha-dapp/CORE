00aa9268e72345ca1e193fbd6649d18ce235fd3d
Spandan Barve <spandan567@gmail.com>
2026-03-17 10:58:58 +0530

    added shop hook export

--- packages/lib/react-sdk/hooks.ts
+++ packages/lib/react-sdk/hooks.ts
@@ -3,6 +3,7 @@ export * from "./src/hooks/chapter";
 export * from "./src/hooks/course";
 export * from "./src/hooks/game";
 export * from "./src/hooks/job";
+export * from "./src/hooks/shop";
 export * from "./src/hooks/social";
 export * from "./src/hooks/users";
 export * from "./src/hooks/wallet";
