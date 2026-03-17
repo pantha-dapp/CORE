20869c6a95c2ef804f9af16384539e76449b29e0
Spandan Barve <spandan567@gmail.com>
2026-03-17 09:38:55 +0530

    upgrade ERC20 implementation to use ERC20Permit

--- packages/contracts/src/PanthaToken.sol
+++ packages/contracts/src/PanthaToken.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PanthaToken is ERC20, Ownable {
+contract PanthaToken is ERC20, Ownable, ERC20Permit {
     constructor(
         uint256 initialSupply_
-    ) ERC20("Pantha", "PANTHA") Ownable(msg.sender) {
+    ) ERC20("Pantha", "PANTHA") ERC20Permit("PanthaToken") Ownable(msg.sender) {
         _mint(msg.sender, initialSupply_ * (10 ** decimals()));
     }
 }
