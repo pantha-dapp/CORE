e5c1baf4b852113bf5e5ac9aeb37086f093873d9
Spandan Barve <spandan567@gmail.com>
2026-03-17 10:15:00 +0530

    create contract version table

---
+++ packages/server/lib/db/schema/runtime.ts
@@ -0,0 +1,9 @@
+import * as t from "drizzle-orm/sqlite-core";
+import { tEvmAddress, timestamps } from "../helpers.base";
+
+export const contractVersions = t.sqliteTable("token_versions", {
+	id: t.int().primaryKey({ autoIncrement: true }),
+	type: t.text("type", { enum: ["pxp"] }).notNull(),
+	contractAddress: tEvmAddress().notNull(),
+	createdAt: timestamps.createdAt,
+});
