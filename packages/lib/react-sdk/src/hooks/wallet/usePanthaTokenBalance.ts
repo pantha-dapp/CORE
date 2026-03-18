0a23c28ebf73fa42eef4aa591b6a7edc208fa0f4
Spandan Barve <spandan567@gmail.com>
2026-03-18 06:27:53 +0530

    improved error handling in usePanthaTokenBalance

--- packages/lib/react-sdk/src/hooks/wallet/usePanthaTokenBalance.ts
+++ packages/lib/react-sdk/src/hooks/wallet/usePanthaTokenBalance.ts
@@ -9,11 +9,21 @@ export function usePanthaTokenBalance() {
 		queryFn: async () => {
 			if (!contracts || !wallet) throw new Error("not connected");
 
+			// const balance = await contracts.$publicClient.readContract({
+			// 	abi: contracts.PanthaToken.abi,
+			// 	address: contracts.PanthaToken.address,
+			// 	functionName: "balanceOf",
+			// 	args: [wallet.account.address],
+			// });
+
 			const balance = await contracts.PanthaToken.read.balanceOf([
 				wallet.account.address,
 			]);
+			const decimals = await contracts.PanthaToken.read.decimals();
 
-			return balance;
+			const humanReadableBalance = Number(balance) / 10 ** decimals;
+
+			return { balanceBps: balance, balanceHuman: humanReadableBalance };
 		},
 	});
 }
