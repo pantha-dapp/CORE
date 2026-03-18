fe3691bc2e2136d9d2188a627e5222751855c5ce
Spandan Barve <spandan567@gmail.com>
2026-03-19 01:45:31 +0530

    fix purchase shop item functionality

---
+++ packages/lib/react-sdk/src/hooks/shop/usePurchaseShopItem.ts
@@ -0,0 +1,75 @@
+import { eip712signature } from "@pantha/contracts";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { parseResponse } from "hono/client";
+import { usePanthaContext } from "../../context/PanthaProvider";
+import { useShopItems } from "./useShopItems";
+
+export function usePurchaseShopItem() {
+	const { wallet, api, contracts } = usePanthaContext();
+	const queryClient = useQueryClient();
+	const { data: shopItems, isLoading: shopItemsLoading } = useShopItems();
+
+	return useMutation({
+		mutationKey: ["purchaseShopItem", wallet?.account.address],
+		mutationFn: async (args: { itemId: string }) => {
+			const { itemId } = args;
+			if (!wallet || !contracts) throw new Error("not connected");
+
+			while (shopItemsLoading) {
+				await new Promise((resolve) => setTimeout(resolve, 100));
+			}
+
+			const currentNonce = await contracts.PanthaToken.read.nonces([
+				wallet.account.address,
+			]);
+
+			const item = shopItems?.items.find((i) => i.id === itemId);
+			if (!item) throw new Error("Item not found");
+
+			const deadline = Math.floor(Date.now() / 1000) + 600;
+
+			const signature = await eip712signature(contracts, "PanthaToken", {
+				types: {
+					Permit: [
+						{ name: "owner", type: "address" },
+						{ name: "spender", type: "address" },
+						{ name: "value", type: "uint256" },
+						{ name: "nonce", type: "uint256" },
+						{ name: "deadline", type: "uint256" },
+					],
+				},
+				primaryType: "Permit",
+				message: {
+					owner: wallet.account.address,
+					spender: contracts.PanthaShop.address,
+					value: item.priceBps,
+					nonce: Number(currentNonce),
+					deadline: deadline,
+				},
+			});
+
+			const response = await parseResponse(
+				api.rpc.shop.buy.$post({
+					query: {
+						itemId,
+						signature,
+						deadline: deadline.toString(),
+					},
+				}),
+			);
+
+			if (!response.success) {
+				throw new Error(response.error || "Purchase failed");
+			}
+			const receipt = await contracts.$publicClient.waitForTransactionReceipt({
+				hash: response.data.txHash,
+			});
+
+			if (receipt.status === "success") {
+				return true;
+			} else {
+				return false;
+			}
+		},
+	});
+}
