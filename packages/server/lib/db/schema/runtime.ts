8ff30d0eace6e5d1bf2999ef413c6c64a68dc50e
Spandan Barve <spandan567@gmail.com>
2026-03-18 07:01:01 +0530

    update contract version schema to include shop type

--- packages/server/lib/db/schema/runtime.ts
+++ packages/server/lib/db/schema/runtime.ts
@@ -3,7 +3,7 @@ import { tEvmAddress, timestamps } from "../helpers.base";
 
 export const contractVersions = t.sqliteTable("token_versions", {
 	id: t.int().primaryKey({ autoIncrement: true }),
-	type: t.text("type", { enum: ["pxp"] }).notNull(),
+	type: t.text("type", { enum: ["pxp", "shop", "token"] }).notNull(),
 	contractAddress: tEvmAddress().notNull(),
 	createdAt: timestamps.createdAt,
 });
