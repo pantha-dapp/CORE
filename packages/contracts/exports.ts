19ad29daa33a8a3a87ef430c09823aec7ad602a4
Spandan Barve <spandan567@gmail.com>
2026-03-20 13:14:09 +0530

    added encryption utilities to exports

--- packages/contracts/exports.ts
+++ packages/contracts/exports.ts
@@ -1,4 +1,7 @@
 export { getContracts, type PanthaContracts } from "./services/contracts";
 export * from "./services/utils/bytes";
 export { eip712signature } from "./services/utils/eip712";
+export { decrypt, encrypt } from "./services/utils/encryption";
+export { encapsulate } from "./services/utils/kem";
+export { walletKeyGen } from "./services/utils/keygen";
 export { createNFTMetadata, metadataToBuffer } from "./services/utils/nft";
