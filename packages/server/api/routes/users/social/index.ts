3ed6be5123bb3a7f117c8529518b75405e506d89
Spandan Barve <spandan567@gmail.com>
2026-03-20 16:39:14 +0530

    fix keygen route validation and submission functionality

--- packages/server/api/routes/users/social/index.ts
+++ packages/server/api/routes/users/social/index.ts
@@ -1,4 +1,55 @@
+import { tryCatch } from "@pantha/shared";
+import { zHex } from "@pantha/shared/zod";
 import { Hono } from "hono";
+import z from "zod";
+import { respond } from "../../../../lib/utils/respond";
+import { authenticated } from "../../../middleware/auth";
+import { validator } from "../../../middleware/validator";
 import type { RouterEnv } from "../../types";
 
-export default new Hono<RouterEnv>();
+export default new Hono<RouterEnv>().post(
+	"/keygen",
+	authenticated,
+	validator(
+		"json",
+		z.object({
+			seedSalt: zHex(),
+			challengeSalt: zHex(),
+			publicKey: zHex(),
+			signature: zHex(),
+		}),
+	),
+	async (ctx) => {
+		const { contracts } = ctx.var.appState;
+		const { seedSalt, challengeSalt, publicKey, signature } =
+			ctx.req.valid("json");
+
+		const txHash = await tryCatch(
+			contracts.PanthaKeyStore.write.registerKeygenData([
+				seedSalt,
+				challengeSalt,
+				publicKey,
+				signature,
+			]),
+		);
+
+		if (txHash.error) {
+			console.error(
+				"Failed to submit keygen data registration txn",
+				txHash.error,
+			);
+			return respond.err(
+				ctx,
+				"Failed to submit keygen data registration txn",
+				500,
+			);
+		}
+
+		return respond.ok(
+			ctx,
+			{ txHash: txHash.data },
+			"Keygen data registration txn submitted",
+			201,
+		);
+	},
+);
