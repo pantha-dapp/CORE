1c49ffbefd6b93184871d44a0f38ef05c3a1ce43
Spandan Barve <spandan567@gmail.com>
2026-03-20 17:54:26 +0530

    add social media schema for feedpost data

--- packages/server/lib/db/schema/social.ts
+++ packages/server/lib/db/schema/social.ts
@@ -11,3 +11,14 @@ export const followings = t.sqliteTable(
 	},
 	(table) => [t.primaryKey({ columns: [table.follower, table.following] })],
 );
+
+export const feedpost = t.sqliteTable(
+	"feedpost",
+	{
+		follower: tEvmAddress().notNull(),
+		following: tEvmAddress().notNull(),
+
+		...timestamps,
+	},
+	(table) => [t.primaryKey({ columns: [table.follower, table.following] })],
+);
