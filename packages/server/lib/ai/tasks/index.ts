0ec29520689c7db9353be65471545a9e19095323
Spandan Barve <spandan567@gmail.com>
2026-03-27 23:13:28 +0530

    added chat ai response generator import

--- packages/server/lib/ai/tasks/index.ts
+++ packages/server/lib/ai/tasks/index.ts
@@ -2,6 +2,7 @@ import clarificationQuestionGenerator from "./clarificationQuestionGenerator";
 import courseSelectionEvaluator from "./courseSelectionEvaluator";
 import generateAnswerExplanation from "./generateAnswerExplanation";
 import generateChapterPages from "./generateChapterPages.legacy";
+import generateChatAiResponse from "./generateChatAiResponse";
 import generateIdealCourseDescriptor from "./generateIdealCourseDescriptor";
 import generateNewCourseSkeleton from "./generateNewCourseSkeleton";
 import intentClarification from "./intentClarification";
@@ -12,6 +13,7 @@ const tasks = {
 	courseSelectionEvaluator,
 	generateAnswerExplanation,
 	generateChapterPages,
+	generateChatAiResponse,
 	generateIdealCourseDescriptor,
 	generateNewCourseSkeleton,
 	intentClarification,
