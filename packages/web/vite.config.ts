2c4db7d096b1888b5e2e1dc7490eebfb0b0f191d
Spandan Barve <spandan567@gmail.com>
2026-03-29 20:25:34 +0530

    add vite build options

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: "dist",
+	},
 	resolve: {
 		dedupe: ["react", "react-dom"],
 	},
