bfb3418ef3e69660073e0ea0444d1cf4f5d7e044
Abhishek Chaudhary <abhishek97208@gmail.com>
2026-03-31 11:08:27 +0530

    import { PanthaProvider as PanthaProviderBase, useEvent } from "@pantha/

--- packages/web/src/shared/contexts/AppWrapper.tsx
+++ packages/web/src/shared/contexts/AppWrapper.tsx
@@ -1,4 +1,5 @@
-import { PanthaProvider as PanthaProviderBase } from "@pantha/react";
+import { PanthaProvider as PanthaProviderBase, useEvent } from "@pantha/react";
+import { toast } from "sonner";
 import { useWalletClient } from "wagmi";
 
 export function PanthaProvider({ children }: { children: React.ReactNode }) {
@@ -6,7 +7,15 @@ export function PanthaProvider({ children }: { children: React.ReactNode }) {
 
 	return (
 		<PanthaProviderBase apiBaseUrl={"/"} wallet={wallet}>
-			{children}
+			<NotificationsWrapper>{children}</NotificationsWrapper>
 		</PanthaProviderBase>
 	);
+}
+
+function NotificationsWrapper({ children }: { children: React.ReactNode }) {
+	useEvent("dm:new", ({ from }) => {
+		toast(`New message from ${from}`, {});
+	});
+
+	return <>{children}</>;
 }
