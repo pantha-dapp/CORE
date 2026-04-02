7d0e0dd8a668b888dba3576d2978b1260f4b7e51
Abhishek Chaudhary <abhishek97208@gmail.com>
2026-04-02 11:04:12 +0530

    add enabled check for wallet and jwt exists

--- packages/lib/react-sdk/src/hooks/shop/useShopItems.ts
+++ packages/lib/react-sdk/src/hooks/shop/useShopItems.ts
@@ -5,11 +5,12 @@ import { usePanthaContext } from "../../context/PanthaProvider";
 
 export function useShopItems() {
 	const { api, wallet } = usePanthaContext();
+	const enabled = !!wallet && !!api.jwtExists;
 
 	return useQuery({
 		queryKey: ["shopItems"],
 		queryFn: async () => {
-			if (!wallet) {
+			if (!enabled) {
 				throw new Error("not connected");
 			}
 
@@ -18,5 +19,6 @@ export function useShopItems() {
 			return response.data;
 		},
 		staleTime: 20 * MINUTE,
+		enabled,
 	});
 }
